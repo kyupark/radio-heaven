51889813e4dc5d3b1ef4b75028e951cc722ada86
Kyu Park <park@kyu.co>
2012-09-03 20:32:24 -0400

    update

--- rh_podcast_download.rb
+++ rh_podcast_download.rb
@@ -1,16 +1,16 @@
 require 'date'
 
-def url(d)
-  year = d.year
-  month = d.month
-  day = d.day
-  url = "http://down-drm.kbs.gscdn.com/mp3/2fm/heaven_#{year}#{month}#{day}_down.mp3"
+def argument(d)
+  date = d.strftime('%Y%m%d')
+  
+  puts "http://down-drm.kbs.gscdn.com/mp3/2fm/heaven_#{date}_down.mp3"
+  "http://down-drm.kbs.gscdn.com/mp3/2fm/heaven_#{date}_down.mp3 > podcast/heaven_#{date}_down.mp3"
 end
 
 d = Date.new(2011,11,06)
 
 until d < Date.new(2008,4,21)
-  `curl #{url(d)}`
+  `curl -C - #{argument(d)}`
   d -= 1
 end
 
